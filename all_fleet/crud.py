0ed1349af6a2af371b89bdec5f278bd2a03a52ff
Dimitrii-Kozlov <demetra_n@front.ru>
2025-08-04 15:56:44 +0300

    add Airline, AircraftType, Aircraft router

--- all_fleet/crud.py
+++ all_fleet/crud.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
 # from jwt.exceptions import InvalidTokenError
+from pydantic import ValidationError
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.future import select
 
@@ -25,3 +26,20 @@ class AircraftTypeCRUD(BaseCRUD):
 
 class AircraftCRUD(BaseCRUD):
     model = Aircraft
+
+    @classmethod
+    async def add(cls, registration_no, airline_id, aircraft_type_id):
+        airline = await AirlineCRUD.find_one_or_none_by_id(airline_id)
+        aircraft_type = await AircraftTypeCRUD.find_one_or_none_by_id(aircraft_type_id)
+        if not airline or aircraft_type:
+            raise HTTPException(status_code=404, detail="Airline or aircraft type not found")
+        async with async_session_maker() as session:
+            async with session.begin():
+                new_instance = cls.model(registration_no=registration_no, airline_id=airline_id, aircraft_type_id=aircraft_type_id)
+                session.add(new_instance)
+                try:
+                    await session.commit()
+                except SQLAlchemyError as e:
+                    await session.rollback()
+                    raise e
+                return new_instance
--- all_fleet/router.py
+++ all_fleet/router.py
@@ -3,7 +3,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import Depends, APIRouter, Body
 from pydantic import Field
 
-from all_fleet.crud import AirlineCRUD
+from all_fleet.crud import AirlineCRUD, AircraftTypeCRUD, AircraftCRUD
 from all_fleet.schemas import AirlineRead, AirlineCreate, AircraftTypeRead, AircraftTypeCreate, AircraftRead, AircraftCreate
 
 
@@ -35,6 +35,60 @@ async def create_airline(airline: AirlineCreate):
 
 @router.delete("/airlines/{id}/delete")
 # async def delete_user_by_id(id: int, user=Depends(get_current_active_user)):
-async def delete_user_by_id(id: int):
+async def delete_airline_by_id(id: int):
     result = await AirlineCRUD.delete(id)
+    return result
+
+
+@router.get("/aircraft_type/all", response_model=list[AircraftTypeRead])
+# async def get_all_users(user=Depends(get_current_active_user)):
+async def get_all_aircraft_types():
+    aircraft_types = await AircraftTypeCRUD.find_all()
+    return [AircraftTypeRead.from_orm(aircraft_type) for aircraft_type in aircraft_types]
+
+
+@router.get("/aircraft_type/{id}", response_model=AircraftTypeRead)
+# async def get_airline_by_id(id: int, user=Depends(get_current_active_user)):
+async def get_aircraft_type_by_id(id: int):
+    result = await AircraftTypeCRUD.find_one_or_none_by_id(id)
+    return result
+
+
+@router.post("/aircraft_type/create", response_model=AircraftTypeRead)
+async def create_aircraft_type(aircraft_type: AircraftTypeCreate):
+    aircraft_type_db = await AircraftTypeCRUD.add(**aircraft_type.dict())
+    return aircraft_type_db
+
+
+@router.delete("/aircraft_type/{id}/delete")
+# async def delete_user_by_id(id: int, user=Depends(get_current_active_user)):
+async def delete_aircraft_type_by_id(id: int):
+    result = await AircraftTypeCRUD.delete(id)
+    return result
+
+
+@router.get("/aircraft/all", response_model=list[AircraftRead])
+# async def get_all_users(user=Depends(get_current_active_user)):
+async def get_all_aircrafts():
+    aircrafts = await AircraftCRUD.find_all()
+    return [AircraftRead.from_orm(aircraft) for aircraft in aircrafts]
+
+
+@router.get("/aircraft/{id}", response_model=AircraftRead)
+# async def get_airline_by_id(id: int, user=Depends(get_current_active_user)):
+async def get_aircraft_by_id(id: int):
+    result = await AircraftCRUD.find_one_or_none_by_id(id)
+    return result
+
+
+@router.post("/aircraft/create", response_model=AircraftRead)
+async def create_aircraft(aircraft: AircraftCreate):
+    aircraft_db = await AircraftCRUD.add(**aircraft.dict())
+    return aircraft_db
+
+
+@router.delete("/aircraft/{id}/delete")
+# async def delete_user_by_id(id: int, user=Depends(get_current_active_user)):
+async def delete_aircraft_by_id(id: int):
+    result = await AircraftCRUD.delete(id)
     return result
