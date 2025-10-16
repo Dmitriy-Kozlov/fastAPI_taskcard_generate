93954cf49930047b7657869145663bc539faeeae
Dimitrii-Kozlov <demetra_n@front.ru>
2025-10-16 15:13:58 +0300

    add pages: aircraft types

--- static/js/airlines.js
+++ static/js/airlines.js
@@ -1,13 +1,7 @@
 let airlines = [];
+let aircraftTypes = [];
+let aircrafts = [];
 const token = localStorage.getItem('authToken');
-const formEl = document.getElementById('airlineForm');
-
-//const form = document.createElement('form');
-//form.id = 'airlineForm';
-//form.innerHTML = `
-//  <input type="text" id="airlineName" placeholder="Airline name" required>
-//  <button type="submit">Add Airline</button>
-//`;
 
 async function fetchAirlines() {
     try {
@@ -87,34 +81,6 @@ function airlinesTable() {
         });
 }
 
-//formEl.addEventListener('submit', async (e) => {
-//    e.preventDefault();
-//    const name = document.getElementById('airlineName').value.trim();
-//    if (!name) return;
-//
-//    try {
-//        const response = await fetch('/fleet/airlines/create', {
-//            method: 'POST',
-//            headers: { 'Content-Type': 'application/json',
-//                     'Authorization': `Bearer ${authToken}`
-//            },
-//            body: JSON.stringify({ airline: name })
-//        });
-//
-//        if (!response.ok) throw new Error('Failed to create airline');
-//
-//        showToast('Airline created successfully');
-//        formEl.reset();
-//        await fetchAirlines();
-//    } catch (error) {
-//        showToast('Error creating airline', true);
-//        console.error(error);
-//    }
-//});
-
-
-fetchAirlines();
-
 
 async function deleteAirline(airlineId) {
     if (!confirm('Are you sure you want to delete this airline?')) {
@@ -141,3 +107,133 @@ async function deleteAirline(airlineId) {
         showToast('Failed to delete airline');
     }
 }
+
+async function fetchAircraftTypes() {
+    try {
+        const response = await fetch('/fleet/aircraft_type/all',{
+            headers: {
+            'Authorization': `Bearer ${authToken}`
+        },
+        });
+       if (response.status === 401) {
+            localStorage.removeItem("authToken");
+            window.location.href='/pages/auth';
+        }
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(`${errorData.detail}`);
+        }
+
+        aircraftTypes = await response.json();
+        aircraftTypesTable();
+    } catch (error) {
+        console.error('Fetch error:', error);
+        showToast(`Failed to load aircraft types ${error}`);
+    }
+}
+
+function aircraftTypesTable() {
+    const aircraftTypesTableBody = document.getElementById('aircraftTypesTableBody');
+    aircraftTypesTableBody.innerHTML = '';
+
+    aircraftTypes.forEach(aircraftType => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${aircraftType.id}</td>
+            <td>${aircraftType.aircraft_type}</td>
+            <td>
+                <button class="btn-delete" onclick="deleteAircraftType(${aircraftType.id})">Delete</button>
+            </td>
+        `;
+        aircraftTypesTableBody.appendChild(row);
+    });
+
+
+    const aircraftTypesFormRow = document.createElement('tr');
+        aircraftTypesFormRow.innerHTML = `
+            <td colspan="3">
+                <form id="aircraftTypesForm">
+                    <input type="text" id="aircraftTypesName" placeholder="Aircraft Type" required>
+                    <button type="submit">Add Aircraft Type</button>
+                </form>
+            </td>
+        `;
+        aircraftTypesTableBody.appendChild(aircraftTypesFormRow);
+        const aircraftTypesForm = document.getElementById('aircraftTypesForm');
+        aircraftTypesForm.addEventListener('submit', async (e) => {
+            e.preventDefault();
+            const name = document.getElementById('aircraftTypesName').value.trim();
+            if (!name) return;
+
+            try {
+                const response = await fetch('/fleet/aircraft_type/create', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json',
+                             'Authorization': `Bearer ${authToken}`
+                    },
+                    body: JSON.stringify({ aircraft_type: name })
+                });
+
+                if (!response.ok) throw new Error('Failed to create aircraft type');
+
+                showToast('Aircraft type created successfully');
+                aircraftTypesForm.reset();
+                await fetchAircraftTypes();
+            } catch (error) {
+                showToast('Error creating aircraft type', true);
+                console.error(error);
+            }
+        });
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const buttons = document.querySelectorAll('.tab-btn');
+  const contents = document.querySelectorAll('.tab-content');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const targetTab = button.getAttribute('data-tab');
+
+      // Убираем активность у всех
+      buttons.forEach(btn => btn.classList.remove('active'));
+      contents.forEach(content => content.classList.remove('active'));
+
+      // Активируем нужную вкладку
+      button.classList.add('active');
+      document.getElementById(targetTab).classList.add('active');
+    });
+  });
+});
+
+
+async function deleteAircraftType(aircraftTypeId) {
+    if (!confirm('Are you sure you want to delete this aircraft type?')) {
+        return;
+    }
+
+    try {
+        const response = await fetch(`/fleet/aircraft_type/${aircraftTypeId}/delete`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${authToken}`
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to delete aircraft type');
+        }
+
+        await fetchAircraftTypes(); // Refresh the table
+        showToast('Aircraft type deleted successfully');
+    } catch (error) {
+        console.error('Delete error:', error);
+        showToast('Failed to delete aircraft type');
+    }
+}
+
+
+fetchAirlines();
+fetchAircraftTypes();
